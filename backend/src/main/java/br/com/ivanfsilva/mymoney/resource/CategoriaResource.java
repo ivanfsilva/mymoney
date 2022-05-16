af9d3642e89477555979ca7d6fa0dd37f6a01c45
ivanfsilva <ivanfs27@gmail.com>
2022-05-16 13:45:20 -0300

    search category by id

--- backend/src/main/java/br/com/ivanfsilva/mymoney/resource/CategoriaResource.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoney/resource/CategoriaResource.java
@@ -6,6 +6,7 @@ import br.com.ivanfsilva.mymoney.model.Categoria;
 import br.com.ivanfsilva.mymoney.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+        return this.categoriaRepository.findById(codigo).orElse(null);
     }
 
 }
