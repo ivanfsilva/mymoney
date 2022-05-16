63cdd630b481bb58ab9f66ea1b04b22e991379ec
ivanfsilva <ivanfs27@gmail.com>
2022-05-16 13:27:51 -0300

    resource API CategoryResource implemented

---
+++ backend/src/main/java/br/com/ivanfsilva/mymoney/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.com.ivanfsilva.mymoney.resource;
+
+import java.util.List;
+
+import br.com.ivanfsilva.mymoney.model.Categoria;
+import br.com.ivanfsilva.mymoney.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+
+}
