d52604556d383d2de9dd8d8470df7d4af37b2a91
ivanfsilva <ivanfs27@gmail.com>
2022-05-19 14:33:05 -0300

    person search

--- backend/src/main/java/br/com/ivanfsilva/mymoney/repository/PessoaRepository.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoney/repository/PessoaRepository.java
@@ -1,8 +1,10 @@
 package br.com.ivanfsilva.mymoney.repository;
 
 import br.com.ivanfsilva.mymoney.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
+    public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
