466ec5d9793cde8a312dd1aee7aef410e487310b
ivanfsilva <ivanfs27@gmail.com>
2022-05-19 14:24:05 -0300

    switching OAuth 2 and Basic Security with profiles

--- backend/src/main/java/br/com/ivanfsilva/mymoney/config/ResourceServerConfig.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoney/config/ResourceServerConfig.java
@@ -9,6 +9,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,8 +25,10 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
+@Profile("oauth-security")
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 
     @Override
@@ -34,9 +37,9 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
         http.authorizeRequests()
                 .antMatchers("/categorias").permitAll()
                 .anyRequest().authenticated()
-                .and()
+            .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+            .and()
                 .csrf().disable()
                 .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
     }
